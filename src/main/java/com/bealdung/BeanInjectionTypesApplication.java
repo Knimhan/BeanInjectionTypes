fa3e705e57f94dd1308f976f30296fd602238be1
Kumudini Nimhan <kumudininimhan@Kumudinis-MacBook-Pro.local>
2017-03-09 16:19:08 +0530

    project setup

---
+++ src/main/java/com/bealdung/BeanInjectionTypesApplication.java
@@ -0,0 +1,12 @@
+package com.bealdung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanInjectionTypesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanInjectionTypesApplication.class, args);
+	}
+}
